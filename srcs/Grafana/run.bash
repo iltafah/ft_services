746ae0e70b5f5db9d7c7aa6609affa58b261967e
Ilias Tafah <iltafah@e2r9p8.1337.ma>
2021-02-12 15:53:45 +0100

    finished everything and created all the dashboards within grafana

--- srcs/Grafana/run.bash
+++ srcs/Grafana/run.bash
@@ -1,2 +1,8 @@
-telegraf -config etc/telegraf.conf && ./grafana/bin/grafana-server -homepath /grafana
+#/grafana/bin/grafana-server -homepath /grafana
+
+#mv /tmp/grafana.db /grafana/data/grafana.db
+/usr/bin/supervisord -c /etc/supervisord.conf
+
+
+#telegraf -config etc/telegraf.conf && ./grafana/bin/grafana-server -homepath /grafana
 #sh
